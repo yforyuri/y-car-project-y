0272ff452876eaa37c3e6fa44c30284f8fcc9d3d
yforyuri <yforyurirho@gmail.com>
2019-09-30 23:06:40 +0900

    [Ycar - reservation for passenger v.1.1] basic setting of spring boot
    190930
    start spring boot project

---
+++ Ycar-rsv-boot/src/main/java/com/ycar/reservation/YcarRsvBootApplication.java
@@ -0,0 +1,13 @@
+package com.ycar.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YcarRsvBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YcarRsvBootApplication.class, args);
+	}
+
+}
---
+++ Ycar-rsv-boot/src/test/java/com/ycar/reservation/YcarRsvBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ycar.reservation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YcarRsvBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
